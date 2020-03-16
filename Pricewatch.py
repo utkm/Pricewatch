22dd678095f1501018328965c62dd013497abf19
Utkarsh M <m.utkarsh10@gmail.com>
2020-03-16 16:05:00 -0400

    email verification

--- Pricewatch.py
+++ Pricewatch.py
@@ -11,10 +11,10 @@ canada_computers = "https://www.canadacomputers.com/product_info.php?cPath="
 
 URL = input('Please enter a Product URL from either Best Buy Canada, Canada Computers or Amazon Canada: ')
 user_email = input('Please enter your email that you would like to be contacted at: ')
-
 if not fullmatch('\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?', user_email):
-    exit('You have entered an INVALID email')
-    
+    exit('You have entered an INVALID email!')
+
+
 headers = {
     'User_Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/75.0.3770.100 Safari/537.36'
