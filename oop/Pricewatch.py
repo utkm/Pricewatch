07bc8222056f821c6cdd3b5120e2388831441359
Utkarsh M <m.utkarsh10@gmail.com>
2021-12-23 16:19:36 -0500

    fix price checking logic

--- oop/Pricewatch.py
+++ oop/Pricewatch.py
@@ -6,25 +6,6 @@ from time import sleep
 from bs4 import BeautifulSoup
 from CanadaComp import CanadaComputers
 
-def send_mail(email_addr, product_title, product_price, product_URL):
-    server = SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-    server.login('m.utkarsh10@gmail.com', 'fiwjlmehyuoobvvc')
-
-    subject = "The Moment You've been waiting for: The Price Has Decreased!"
-    body = 'Hey!\nThe Price of ' + product_title + " has finally decreased to $" + str(product_price)+ "!\n\nClick the link: "\
-            + product_URL + " to claim your deal now!"
-
-    msg = f"Subject: {subject}\n\n{body}"
-
-    server.sendmail('m.utkarsh10@gmail.com', user_email, msg)
-    print("Email has been sent")
-    server.quit()
-    exit()
-
-
 if __name__ == "__main__":
     best_buy = "https://www.bestbuy.ca/en-ca/"
     amazon = "https://www.amazon.ca/"
@@ -44,13 +25,16 @@ if __name__ == "__main__":
     if not fullmatch('\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?', user_email):
         exit('You have entered an INVALID email!')
 
+
     if best_buy in URL:
         pass
     elif amazon in URL:
         pass
     elif canada_computers in URL:
         cc = CanadaComputers(URL, headers, user_email)
-        cc.cc_check_price()
+        while 1:
+            cc.cc_check_price(99999)
+            sleep(86400)
         
     else:
         print("Something's not working... Try to re-enter a link from either Best Buy Canada,"
