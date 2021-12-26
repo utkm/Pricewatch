6caf13154a8e915bf2524582b1c810fb6718dedf
Utkarsh M <m.utkarsh10@gmail.com>
2021-12-25 22:05:52 -0500

    fixed best buy!! (dynamic website)
    used requests-html

--- oop/Bestbuy.py
+++ oop/Bestbuy.py
@@ -1,37 +1,37 @@
-from requests import get
-from smtplib import SMTP
-from bs4 import BeautifulSoup
-from Pricewatch import send_mail
+import requests
+from Mail import SendMail
+from requests_html import HTMLSession
 
 class BestBuy():
     def __init__(self, URL, headers, user_email):
         self.URL = URL
         self.headers = headers
+        self.user_email = user_email
 
-    def bb_check_price(self):
-        page = get(self.URL, headers=self.headers)
-        page_soup = BeautifulSoup(page.content, "html.parser")
-
-        bb_title = page_soup.find("div", {"class": "x-product-detail-page"})
-        bb_title = bb_title.h1.text
-
+    def bb_check_price(self, curr_price):
         try:
-            bb_price = page_soup.find("div", {"class": "price_FHDfG large_3aP7Z"}).get_text()
-            if len(bb_price) == 6:
-                bb_price = float(bb_price[1:4])
-            # turns $28999 to 289
-            elif len(bb_price) == 8:
-                bb_price = int(bb_price[1]) + float(bb_price[3:6])
-            elif len(bb_price) == 5:
-                bb_price = float(bb_price[1:3])
+            session = HTMLSession()
+            response = session.get(self.URL)
+            
+        except requests.exceptions.RequestException as e:
+            print(e)
 
-            sale_price = bb_price
-            if sale_price <= bb_price:
-                send_mail(self.user_email, bb_title, bb_price, self.URL)
-            else:
-                print("Thank you for using our service. We'll get back to you shortly!")
+        self.product_title =  response.html.find('title')
+        self.product_title = self.product_title[0].text
+        # print(title)
+        
+        self.product_price = response.html.find(".price_FHDfG")
+        self.product_price = self.product_price[0].text
+        self.product_price = float(self.product_price[1:-2])*1.0 + float(self.product_price[-2:])*0.01
+        # print(product_price)
 
-        except AttributeError:
-            print("Hmmm. Try to re-enter the URL of a product that is not already on sale.")
-        except NameError:
-            print("Hmmm. Try to re-enter the URL of a product that is not already on sale.")
+        mail_to = SendMail(self.user_email, self.product_title, self.product_price, self.URL)
+        # mail_to.send_mail()
+
+        if self.product_price < curr_price and curr_price != 99999:
+            mail_to.send_mail(self.product_title, self.product_price, self.URL)
+        else:
+            curr_price = self.product_price
+            print("Thank you for using our service. We'll get back to you shortly!")
+        
+        return self.product_price
